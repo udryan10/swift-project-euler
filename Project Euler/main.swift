2143074c90a34dee8f7f13fcf0829bab3158a40b
Ryan Grothouse <ryan.grothouse@ge.com>
2014-06-10 21:15:50 -0400

    Initial Commit

---
+++ Project Euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project Euler
+//
+//  Created by Grothouse, Ryan (GE, Appl & Light) on 6/10/14.
+//  Copyright (c) 2014 Grothouse, Ryan (GE, Appl & Light). All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
